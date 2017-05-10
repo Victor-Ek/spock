8b32c2deaffc08c9fdf2fcbf926787db90fa2f27
Itggot-Victor-Ek <victor.ek@itggot.se>
2017-05-10 22:39:48 +0200

    Graph klar (kan om orket finns, förbättas)

--- site/js/main.js
+++ site/js/main.js
@@ -1,9 +1,7 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
-src="js/jquery.flot.min.js";
-
 function graph () {
-  var graphdata = [{data: [ [6, 1300], [7, 1600], [8, 1900], [9, 2100], [10, 2500], [11, 2200], [12, 2000], [13, 1950], [14, 1900], [15, 2000] ],
+  var graphData = [{data: [ [6, 1300], [7, 1600], [8, 1900], [9, 2100], [10, 2500], [11, 2200], [12, 2000], [13, 1950], [14, 1900], [15, 2000] ],
         color: '#71c73e'}];
+
 
         $.plot($('#graph-lines'), graphData, {
       series: {
@@ -19,7 +17,7 @@ function graph () {
       grid: {
           color: '#646464',
           borderColor: 'transparent',
-          borderWidth: 20,
+          borderWidth: 1,
           hoverable: true
       },
       xaxis: {
@@ -31,27 +29,4 @@ function graph () {
       }
   });
 
-  $.plot($('#graph-bars'), graphData, {
-  series: {
-      bars: {
-          show: true,
-          barWidth: 0.9,
-          align: 'center'
-      },
-      shadowSize: 0
-  },
-  grid: {
-      color: '#646464',
-      borderColor: 'transparent',
-      borderWidth: 20,
-      hoverable: true
-  },
-  xaxis: {
-      tickColor: 'transparent',
-      tickDecimals: 2
-  },
-  yaxis: {
-      tickSize: 1000
-  }
-});
 }
