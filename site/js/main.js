a02eb5c326024840f881548eacc526fd8977fd4f
Itggot-Victor-Ek <victor.ek@itggot.se>
2017-05-19 21:32:27 +0200

    en mobil skärm snart klar, hamburgar menyn påbörjad,

--- site/js/main.js
+++ site/js/main.js
@@ -1,8 +1,10 @@
 function graph () {
+
+    // Price
   var graphData = [{data: [ [1, 405], [2, 360], [3, 380], [4, 400], [5, 415], [6, 400], [7, 800], [8, 520], [9, 435], [10, 400] ],
-        color: '#71c73e'}];
+        color: '#00e575'}];
 
-
+// Lines
         $.plot($('#graph-lines'), graphData, {
       series: {
           points: {
@@ -28,12 +30,14 @@ function graph () {
           tickSize: 100
       }
   });
+// Lines
 
+// Bars
   $.plot($('#graph-bars'), graphData, {
     series: {
         bars: {
             show: true,
-            barWidth: .9,
+            barWidth: 0.9,
             align: 'center'
         },
         shadowSize: 0
@@ -52,7 +56,9 @@ function graph () {
         tickSize: 200
     }
 });
+// Bars
 
+//Change Graph
 $('#graph-bars').hide();
 
 $('#lines').on('click', function (e) {
@@ -70,17 +76,24 @@ $('#bars').on('click', function (e) {
     $('#graph-bars').fadeIn().removeClass('hidden');
     e.preventDefault();
 });
-
 }
+//Change Graph
 
+
+//Tooltip
 function showTooltip(x, y, contents) {
     $('<div id="tooltip">' + contents + '</div>').css({
         top: y - 16,
-        left: x + 20
+        left: x + 20,
     }).appendTo('body').fadeIn();
 }
 
 var previousPoint = null;
+
+function removeUndefined(){
+  $('#tooltip').remove();
+  previousPoint = null;
+}
 
 $('#graph-lines, #graph-bars').bind('plothover', function (event, pos, item) {
     if (item) {
@@ -91,8 +104,32 @@ $('#graph-lines, #graph-bars').bind('plothover', function (event, pos, item) {
                 y = item.datapoint[1];
                 showTooltip(item.pageX, item.pageY, y + 'kr kostade det ' + x + ' maj');
         }
-    } else {
-        $('#tooltip').remove();
-        previousPoint = null;
+    }
+    else {
+      $('#tooltip').remove();
+      previousPoint = null;
     }
 });
+//Tooltip
+
+
+function more() {
+    element = document.querySelector(".more_button");
+    console.log(element);
+    element.classList.toggle("more_js");
+
+    while ($('#main-div .sold_items_box_more').length >= 0) {
+
+      element = document.querySelector(".sold_items_box_more");
+      element.classList.remove("sold_items_box_more");
+    }
+}
+//More Button
+
+//Hamburger Menu
+ function hamburger_menu() {
+   element = document.querySelector(".menu_background");
+   console.log(element);
+   element.classList.toggle("menu_background_display");
+ }
+//Hamburger Menu
